fcbfec1f9ad91471e2d368bd62dc96d7850b2806
fedi_bouteraa <fadi.bouteraa@gmail.com>
2021-07-02 09:36:49 +0100

    Initial Commit

---
+++ Onboarding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Onboarding
+//
+//  Created by YellowIT on 2/7/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
